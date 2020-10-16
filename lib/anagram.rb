c81ff8b3fb91ece8960ead79fa3c6b88f84a7269
Maria Castaneda <mariamcastaneda29@gmail.com>
2020-10-15 21:37:29 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,33 @@
-# Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+       @word = word  
+    end
+
+    def match(array)
+        array.select do |x|
+            sorted_array = x.split("").sort
+            sorted_word = @word.split("").sort
+            sorted_array == sorted_word
+        
+
+        end
+
+    end
+
+end
+
+#  To determine if they are anagrams, try determining if they are composed of
+#     the same letters. Remember that you can split a word into an array of letters
+#     using `some_word.split("")`. You can compare two arrays using the `==`. For
+#     example
+
+
+
+
+# listen = Anagram.new("listen")
+# listen.match(%w(enlists google inlets banana))
+
+# # => ["inlets"]
